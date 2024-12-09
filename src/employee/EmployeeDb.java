80e6e97ed74ee060ef48f801bd3e7316aace3bee
suhas-jo <suhas.manjunath81@gmail.com>
2024-12-10 02:09:22 +0530

    add more Employee programs

--- src/employee/EmployeeDb.java
+++ src/employee/EmployeeDb.java
@@ -13,27 +13,27 @@ public class EmployeeDb {
         Address address1 = new Address("123 Main St", "Springfield", "IL", "62701");
         Employee employee1 = new Employee(101,20, "Alice", "Engineering",25000, address1);
         Address address2 = new Address("54 Main St", "Springfield", "IL", "62702");
-        Employee employee2 = new Employee(102,22, "Bob", "HR",25000, address2);
+        Employee employee2 = new Employee(102,22, "Bob", "HR",35000, address2);
         Address address7 = new Address("5 Main St", "Springfield", "IL", "62708");
-        Employee employee7 = new Employee(107,35, "George", "DEV-OPS",25000, address7);
+        Employee employee7 = new Employee(107,35, "George", "DEV-OPS",22000, address7);
 
         Address address8 = new Address("4 Main St", "Springfield", "IL", "62709");
-        Employee employee8 = new Employee(108,30, "Hannah", "Engineering",25000, address8);
+        Employee employee8 = new Employee(108,30, "Hannah", "Engineering",45000, address8);
 
         Address address9 = new Address("5 Main St", "Springfield", "IL", "62708");
-        Employee employee9 = new Employee(109,31, "Ivan", "DEV-OPS",25000, address9);
+        Employee employee9 = new Employee(109,31, "Ivan", "DEV-OPS",55000, address9);
 
         Address address3 = new Address("43 Main St", "Springfield", "IL", "62703");
-        Employee employee3 = new Employee(103,24, "Charlie", "QA",25000, address3);
+        Employee employee3 = new Employee(103,24, "Charlie", "QA",83000, address3);
 
         Address address4 = new Address("9 Main St", "Springfield", "IL", "62705");
-        Employee employee4 = new Employee(104,28, "Diana", "Engineering",25000, address4);
+        Employee employee4 = new Employee(104,28, "Diana", "Engineering",34000, address4);
 
         Address address5 = new Address("8 Main St", "Springfield", "IL", "62706");
-        Employee employee5 = new Employee(105,56, "Edward", "HR",25000, address5);
+        Employee employee5 = new Employee(105,56, "Edward", "HR",546000, address5);
 
         Address address6 = new Address("7 Main St", "Springfield", "IL", "62707");
-        Employee employee6 = new Employee(106,34, "Fiona", "QA",25000, address6);
+        Employee employee6 = new Employee(106,34, "Fiona", "QA",85000, address6);
 
 
 
--- src/streams/EmployeeProblems.java
+++ src/streams/EmployeeProblems.java
@@ -3,10 +3,7 @@ package streams;
 import employee.Employee;
 import employee.EmployeeDb;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class EmployeeProblems {
@@ -17,15 +14,68 @@ public class EmployeeProblems {
         
 //        filterBasedOnDept(employeeList);
 
-//        createListOfEmployeeNames(employeeList);
+        createListOfEmployeeNames(employeeList);
 
 //        sortEmployeesByName(employeeList);
 //        sortEmployeeByAge(employeeList);
 //        calculateAvgAge(employeeList);
 //        findMaxAge(employeeList);
-        findMaxAgeEmployee(employeeList);
+//        findMaxAgeEmployee(employeeList);
 //        groupEmployeesByDept(employeeList);
+//        sumEployeeAge(employeeList);
+//        createCommaSeperatedEmployees(employeeList);
+//        findEmployeeWithMinSalary(employeeList);
+//        removeEmployeeWithSalaryLessThan50k(employeeList);
+//        replaceEmployeeNameWhoseSalLessThan50K(employeeList);
+        divideEmployeeBasedOnOddAndEvenAge(employeeList);
+}
 
+    private static void divideEmployeeBasedOnOddAndEvenAge(List<Employee> employeeList) {
+
+        Map<Boolean,List<Employee>> employeeGroup = employeeList.stream()
+                .collect(Collectors.partitioningBy(employee -> employee.getAge()%2==0));
+        employeeGroup.get(true).forEach(employee -> System.out.println(employee.getName() + "'s age is " + employee.getAge()));
+        employeeGroup.get(false).forEach(employee -> System.out.println(employee.getName() + "'s age is " + employee.getAge()));
+    }
+
+    private static void replaceEmployeeNameWhoseSalLessThan50K(List<Employee> employeeList) {
+
+        employeeList.replaceAll(employee -> employee.getSalary()<50000?new Employee(employee.getId(),employee.getAge(),"AAA",employee.getDepartment(),
+                employee.getSalary(),employee.getAddress()):employee);
+
+        employeeList.forEach(employee -> System.out.println(employee.getName() + " is getting salary " + employee.getSalary()) );
+    }
+
+    private static void removeEmployeeWithSalaryLessThan50k(List<Employee> employeeList) {
+        employeeList.removeIf(employee -> employee.getSalary()<50000);
+        employeeList.forEach(employee -> System.out.println(employee.getName() + " getting salary : " + employee.getSalary()));
+
+        //sort based on based Salary
+        System.out.println("*****Sorted employee based on Salary***********");
+        employeeList.stream().sorted(Comparator.comparing(Employee::getSalary))
+                .forEach(employee -> System.out.println(employee.getName() + " getting salary " + employee.getSalary()));
+    }
+
+    private static void findEmployeeWithMinSalary(List<Employee> employeeList) {
+
+       Optional<Employee> minSalEmployee = employeeList.stream().min(Comparator.comparing(Employee::getSalary));
+
+        minSalEmployee.ifPresent(System.out::println);
+    }
+
+    private static void createCommaSeperatedEmployees(List<Employee> employeeList) {
+        String names = employeeList.stream().map(Employee::getName).collect(Collectors.joining(","));
+        System.out.println(names);
+    }
+
+    private static void sumEployeeAge(List<Employee> employeeList) {
+
+        IntSummaryStatistics intSummaryStatistics = employeeList.stream().mapToInt(Employee::getAge).summaryStatistics();
+
+        System.out.println(intSummaryStatistics.getSum());
+        System.out.println(intSummaryStatistics.getMin());
+        System.out.println(intSummaryStatistics.getMax());
+        System.out.println(intSummaryStatistics.getAverage());
     }
 
 
